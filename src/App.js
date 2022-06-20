9530475ee6b696a032c75bd78979442ceb89b4a4
Joe Daniel Ortiz Morales <isc18350328@gmail.com>
2022-06-19 22:38:08 -0500

    Primer avance de Poke_Api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState, useEffect } from "react";
+import { getAllPokemon, getPokemon} from "./pokemon";
+
 
 function App() {
+
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState("");
+  const [prevUrl, setPrevUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon'
+
+
+  useEffect (() => {
+    async function fetchData(){
+      let response = await getAllPokemon(initialUrl);// Funcion para obetener los pokemones de la API
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      await loadingPokemon(response.results);
+      setLoading(false);
+    }
+    fetchData();
+  },[]);
+
+  const loadingPokemon = async (data) => { //Funcion de carga
+    let _pokemon = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon.url);
+      return pokemonRecord
+    }))
+    setPokemonData(_pokemon)
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? <h1>Cargando...</h1> : 
+        <h1>Obteniendo datos</h1>
+      }
     </div>
   );
 }
